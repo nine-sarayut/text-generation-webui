596a7e09d27631ac1bb5d7782d5daf5dd3d0e84c
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-26 13:21:29 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -1,6 +1,6 @@
 conda config --set auto_activate_base false
 python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --n-gpu-layers 35 --n_gqa 8 --rms_norm_eps 1e-5
-python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --n-gpu-layers 1024 --n_gqa 8 --rms_norm_eps 1e-5
+python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --chat --n-gpu-layers 1024 --n_gqa 8 --rms_norm_eps 1e-5 --listen-port 50099
 python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --n-gpu-layers 1024 --n_gqa 8 --rms_norm_eps 1e-5
 python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --chat --n-gpu-layers 1024
 python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --chat --n-gpu-layers 1024
