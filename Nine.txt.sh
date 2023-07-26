91b2c7b32bd4c88855f376bad12c9369acdf26ac
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-26 15:31:33 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -4,6 +4,7 @@ python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --chat --n-gpu-layers 102
 python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --n-gpu-layers 1024 --n_gqa 8 --rms_norm_eps 1e-5
 python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --chat --n-gpu-layers 1024
 python server.py --model llama-2-70b.ggmlv3.q4_K_M.bin --chat --n-gpu-layers 1024
+conda activate textgen
 python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --chat --n-gpu-layers 1024 --listen-port 50099
 # python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --chat --n-gpu-layers 1024 --listen-port 50099 --gpu-memory 16384
 # python server.py --model llama-2-13b-chat.ggmlv3.q4_0.bin --notebook --n-gpu-layers 1024 --listen-port 50099
